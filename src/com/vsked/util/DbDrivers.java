ba41e2ce22a0431dd96d6f3d176e74001c7e68bb
chousheng <vsked@163.com>
2015-06-04 10:23:38 +0800

    add dynamicInputTypeChange ,fix dbdriver for mysql

--- src/com/vsked/util/DbDrivers.java
+++ src/com/vsked/util/DbDrivers.java
@@ -2,7 +2,7 @@ package com.vsked.util;
 
 public class DbDrivers {
 	
-	public static final String mysqlDriver="org.gjt.mm.mysql.Driver";
+	public static final String mysqlDriver="com.mysql.jdbc.Driver";
 	public static final String mysqlUrl="jdbc:mysql://127.0.0.1:3306/mysql?user=root&password=root&useUnicode=true&characterEncoding=utf-8";
 	
 	public static final String oraclelDriver="oracle.jdbc.OracleDriver";
